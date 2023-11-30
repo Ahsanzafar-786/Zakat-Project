3ecd1ebfc620d2f42037e29ef6e54d6e0fff1473
AZ Software Engineer <tq.bedeveloper3@outlook.com>
2023-11-30 15:52:33 +0500

    wip

--- Noble.web/src/components/Payment/Payment.vue
+++ Noble.web/src/components/Payment/Payment.vue
@@ -6,7 +6,7 @@
                     <div class="page-title-box">
                         <div class="row">
                             <div class="col">
-                                <h4 class="page-title">{{ $t('Payment.Payment') }}</h4>
+                                <h4 class="page-title">{{ $t('Payment.PaymentTitle') }}</h4>
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="javascript:void(0);">{{ $t('Home') }}</a>
                                     </li>
